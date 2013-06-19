24253bc44bd18c0b5c02f35caeaaffe08d3acaa4
Johan van Soest <johan.vansoest@maastro.nl>
2013-06-19 17:47:39 +0200

    Added simple form of creating triples and store to an RDF (XML) file. Need to check the excel file or add regular expressions to clear chemical ID's starting with a whitespace.

--- tripleCreation.js
+++ tripleCreation.js
@@ -1,12 +1,40 @@
-fileLocation = "C:\\Johan\\Repositories\\GitHub\\miils2013toxic\\";
+var fileLocation = "/miils2013toxic/CPDBChemical.csv";
+var stringObj = ui.readFile(fileLocation);
+var lineArr = stringObj.split("\n");
 
-var file = new File(fileLocation + "CPDBChemical.csv");
+//number of columns which define the table header
+var headerRowSize = 2;
+var ourPrefix = "group";
 
-s = rdf.createInMemoryStore()
-rdf.addPrefix(s, "foo", "http://example.org/")
-rdf.addDataProperty(s,
-	"http://example.org/foo",
-	"http://www.w3.org/2000/01/rdf-scheme#label",
-	"Foo"
-)
-rdf.saveRDFN3(s,"miils2013toxic\\cpdb.n3")
+//create the store
+var rdfStore = rdf.createInMemoryStore();
+rdf.addPrefix(rdfStore, "rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+rdf.addPrefix(rdfStore, "rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+rdf.addPrefix(rdfStore, ourPrefix, "http://example.org#");
+
+
+/////////////////////////////////////////////////
+// create functions to use in main loop        //
+/////////////////////////////////////////////////
+
+function createPrefix(object) {
+	return ourPrefix+":"+object;
+}
+
+function getOrCreateCompound(compoundName, labelName) {
+	rdf.addObjectProperty(rdfStore, createPrefix(compoundName), "rdf:type", "rdf:Object");
+	rdf.addDataProperty(rdfStore, createPrefix(compoundName), "rdf:label", labelName);
+}
+
+/////////////////////////////////////////////////
+// loop through every row of the CSV file      //
+/////////////////////////////////////////////////
+for(var i=(0+headerRowSize); i<lineArr.length; i++) {
+	var line = lineArr[i];
+	var colValues = line.split(";");
+	
+	//insert the compounds
+	getOrCreateCompound(colValues[1], colValues[0]);
+}
+rdf.saveRDFXML(rdfStore,"/miils2013toxic/cpdb.rdf")
+rdf.saveRDFN3(rdfStore,"/miils2013toxic/cpdb.n3")
